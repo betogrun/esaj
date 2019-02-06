4a0ed8410f4c0c77b1030012d93d9c36189959aa
Alberto Rocha <betogrun@gmail.com>
2019-02-06 21:25:07 -0200

    Fix forum value type (#20)

--- spec/esaj/forum_spec.rb
+++ spec/esaj/forum_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Esaj::Forum do
   describe '.code_for' do
     it do
-      expect(Esaj::Forum.code_for(:all)).to eq(-1)
+      expect(Esaj::Forum.code_for(:all)).to eq("-1")
     end
   end
 end
